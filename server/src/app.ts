a41d4639da5686cce0b21385bc37279ec5c876a0
Ibrahim-Jarada <ibrahim.jarada@gmail.com>
2022-04-20 04:28:05 +0300

    importing and apply dotenv dep specify api route to use router serving static files from client/bulid folder in production phase create * route with empty void  handler

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,7 +2,10 @@ import express, { Application } from 'express';
 import { join } from 'path';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
+import dotenv from 'dotenv';
 import router from './routes';
+
+dotenv.config();
 
 const app:Application = express();
 app.disable('etag');
@@ -11,9 +14,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(compression());
 
-app.use(express.static(join(__dirname, '..', 'public')));
+app.use('/api/v1/', router);
 
-app.use(router);
+const { NODE_ENV } = process.env;
+if (NODE_ENV === 'production') {
+  app.use(express.static(join(__dirname, '..', 'client', 'build')));
+  app.get('*', () => {});
+}
 
 app.set('port', process.env.PORT || 3000);
 
