97bf046f651dd7a9108edfab3a92af0820292b70
Ibrahim-Jarada <ibrahim.jarada@gmail.com>
2022-04-22 00:43:22 +0300

    create linting scripts and fix linting scripts inside server package.json and enforce check liniting before commits by including linting scripts in pre-commit scripts array

--- server/src/controllers/serverError.ts
+++ server/src/controllers/serverError.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 
+// eslint-disable-next-line no-unused-vars
 const serverError = (err:Error, req:Request, res:Response, next:NextFunction) => {
   res.status(500).json(err);
 };
