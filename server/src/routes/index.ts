65a649d3b584908651e3c22997558ce6367fac93
Ibrahim-Jarada <ibrahim.jarada@gmail.com>
2022-04-22 00:55:54 +0300

    initialize notFound,serverError,logout routes in router

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
-import { signUp, login } from '../controllers';
+import {
+  signUp, login, logout, notFound, serverError,
+} from '../controllers';
 
 const router = Router();
 
 router.post('/signup', signUp);
 router.post('/login', login);
+router.post('/logout', logout);
+router.use(notFound);
+router.use(serverError);
+
 export default router;
