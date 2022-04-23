1211952784e9c56abf200e6f352e60b86441afce
MahmoudJD95 <mld95.doa@gmail.com>
2022-04-23 23:05:43 +0300

    feat: add logo to ligi page

--- client/src/Components/logInComponent.jsx
+++ client/src/Components/logInComponent.jsx
@@ -1,8 +1,10 @@
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
 
-import { useNavigate, Routes, Route } from 'react-router-dom';
-import { Button, TextField } from '@mui/material';
+import {
+  Link,
+} from 'react-router-dom';
+import { Button, TextField, Typography } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 import axios from 'axios';
 
@@ -22,7 +24,11 @@ const useStyles = makeStyles({
   },
   img: {
     height: '100%',
-    width: '40%',
+    width: '50%',
+  },
+  logo: {
+    height: '300px',
+    width: '500px',
   },
   formStyle: {
     display: 'flex',
@@ -76,14 +82,22 @@ function LogInComponent() {
 
       <img className={classes.img} src="https://images.unsplash.com/photo-1606787366850-de6330128bfc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80" alt="img" />
       <form className={classes.formStyle} onSubmit={(e) => handelSubmit(e)}>
+        <img className={classes.logo} src="https://o.remove.bg/downloads/5dd29f37-cdef-4f0a-beed-21df992cc719/Screen_Shot_2022-04-23_at_10.59.53_PM-removebg-preview.png" alt="img" />
         <h1 className={classes.head}>welcome to Instashop</h1>
         <TextField className={classes.textFld} variant="outlined" type="email" placeholder=" Enter your email" onChange={(e) => setEmail(e.target.value)} required />
         <TextField className={classes.textFld} variant="outlined" type="password" placeholder=" Password" onChange={(e) => setPassword(e.target.value)} required />
         {/* {result.body.msg === 'logIn successfully' ? null : <h1>{result.body.msg}</h1>} */}
 
         <Button type="submit" className={classes.signInBtn} variant="contained" size="large">Sign In</Button>
+        <Typography component="h2" style={{ margin: '15px' }}>
+          {' '}
+          Create new account
+          {' '}
+          <a href="/signin">
+            Sign In
+          </a>
+        </Typography>
       </form>
-
     </div>
   );
 }
