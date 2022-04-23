7a09c6d770ef6b69df8ec548379b7c4a664d7834
SalmaIssa96 <salmaissa196@gmail.com>
2022-04-23 23:40:17 +0300

    feat: modify login schema put email inted of name

--- server/src/controllers/validation/logInSchema.ts
+++ server/src/controllers/validation/logInSchema.ts
@@ -1,8 +1,7 @@
 import Joi from 'joi';
 
 const logInValidation = Joi.object({
-
-  name: Joi.string().required(),
+  email: Joi.string().email().required(),
   password: Joi.string().alphanum().min(6).required(),
 });
 
