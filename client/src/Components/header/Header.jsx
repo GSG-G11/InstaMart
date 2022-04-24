7cf29f86e0c253ced3f2a435df9767f5f60b4c51
SalmaIssa96 <salmaissa196@gmail.com>
2022-04-24 23:55:35 +0300

    feat: add drop menu contains logout

--- client/src/Components/header/Header.jsx
+++ client/src/Components/header/Header.jsx
@@ -1,32 +1,56 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import './index.css';
-import { ShoppingCart, AccountCircle } from '@mui/icons-material';
-// import { Menu, MenuItem } from '@mui/material';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import { ShoppingCart, AccountCircle, Logout } from '@mui/icons-material';
 import appIcon from './grocery-cart 3.png';
-// import { useAuth } from '../../useAuth';
-
-// const { user } = useAuth();
+import { useAuth } from '../../useAuth';
 
 function Header() {
-  //   const navigate = useNavigate();
+  const navigate = useNavigate();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const { logout, user } = useAuth();
+  const logoutFunc = () => {
+    logout((error) => {
+      if (!error) {
+        navigate('/');
+      } else {
+        console.log(error);
+      }
+    });
+  };
 
   return (
+
     <div className="header-section">
-      <div className="logo-section">
-        <img src={appIcon} alt="logo-img" className="logo-img" />
-        <p className="logo-name"> Instamart</p>
-      </div>
-      <div className="navigate-div">
-        <Link className="navigate-word" to="/home">
-          Home
-        </Link>
-        <Link className="navigate-word" to="/products">
-          Products
-        </Link>
-        <Link className="navigate-word" to="/dashboard">
-          Dashboard
-        </Link>
+      <div className="left-section">
+        <div className="logo-section">
+          <img src={appIcon} alt="logo-img" className="logo-img" />
+          <p className="logo-name"> Instamart</p>
+        </div>
+        <div className="navigate-div">
+          <Link className="navigate-word" to="/home">
+            Home
+          </Link>
+          <Link className="navigate-word" to="/products">
+            Products
+          </Link>
+          <Link className="navigate-word" to="/dashboard">
+            Dashboard
+          </Link>
+        </div>
       </div>
       <div className="icons-div">
         <Link to="./cart">
@@ -36,30 +60,34 @@ function Header() {
             <p className="products-number">5</p>
           </div>
         </Link>
-        <div className="user-info">
-          <AccountCircle className="shopping-cart-icon" />
-          <p className="user-name">Salma</p>
-          {/* <Menu
-            id="lock-menu"
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            MenuListProps={{
-              'aria-labelledby': 'lock-button',
-              role: 'listbox',
-            }}
-          >
-            <MenuItem key="Log out">{option}</MenuItem>
-          </Menu> */}
-        </div>
 
-        {/* {user ? (
+        {user ? (
           <>
-            <AccountCircle className="shopping-cart-icon" />
-            <p className="user-name">{user.name}</p>
+            <div
+              onClick={handleClick}
+              className="user-info"
+            >
+              <AccountCircle className="shopping-cart-icon" />
+              <p className="user-name">Salma</p>
+            </div>
+            <Menu
+              anchorEl={anchorEl}
+              id="account-menu"
+              className="profile-menu"
+              open={open}
+              onClose={handleClose}
+              onClick={handleClose}
+            >
+              <MenuItem onClick={() => { logoutFunc(); }}>
+                <ListItemIcon>
+                  <Logout fontSize="small" />
+                </ListItemIcon>
+                Logout
+              </MenuItem>
+            </Menu>
 
           </>
-        ) : null} */}
+        ) : null}
       </div>
     </div>
   );
