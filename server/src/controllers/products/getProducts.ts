39e0f8f272e71cf490b639f56d5fda0ed02eca2e
Ibrahim-Jarada <ibrahim.jarada@gmail.com>
2022-04-24 05:53:36 +0300

    feat: get filtered products functionality

--- server/src/controllers/products/getProducts.ts
+++ server/src/controllers/products/getProducts.ts
@@ -1,7 +1,22 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
+import { getProductsValidation } from '../validation';
+import { Product } from '../../database';
 
-const getProducts = (req:Request, res:Response) => {
-  res.json();
+const getProducts = async (req:Request, res:Response, next:NextFunction) => {
+  const { q, categoryId, sort } = req.query;
+  try {
+    await getProductsValidation(req);
+    const dbProducts = await Product.findAll({
+      where: categoryId ? { categoryId: +categoryId } : undefined,
+      order: sort ? [['price', 'DESC']] : undefined,
+    });
+    res.json(dbProducts.filter(({ name }) => !q || name.includes(`${q}`)));
+  } catch (err:any) {
+    if (err.details) {
+      res.status(422).json({ msg: err.details[0].message, status: 422 });
+    }
+    next(err);
+  }
 };
 
 export default getProducts;
