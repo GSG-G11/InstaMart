b41e8b51bd5e2b24c008910a4b0e86356afa1a9e
yousra Jaradh <yousrakhaleel@gmail.com>
2022-04-26 00:51:24 +0300

    perf: remove console.log Relates #53

--- server/src/controllers/serverError.ts
+++ server/src/controllers/serverError.ts
@@ -5,7 +5,6 @@ interface ModError extends Error {
 }
 // eslint-disable-next-line no-unused-vars
 const serverError = (err:ModError, req:Request, res:Response, next:NextFunction) => {
-  console.log(err);
   res.status(err.status || 500).json({ status: (err.status || 500), msg: err.message });
 };
 
