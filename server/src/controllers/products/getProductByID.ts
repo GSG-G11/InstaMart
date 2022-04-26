94413d3b3b456ed187883543fa2afffa0178ba29
SalmaIssa96 <salmaissa196@gmail.com>
2022-04-26 14:04:38 +0300

    fix: fix bug in get productby id route

--- server/src/controllers/products/getProductByID.ts
+++ server/src/controllers/products/getProductByID.ts
@@ -6,13 +6,12 @@ const getProductByID = async (req:Request, res:Response, next:NextFunction) => {
   try {
     const productByID = await Product.findOne({ include: Category, where: { id } });
     if (productByID) {
-      res.json({ success: true, data: productByID });
-    } else {
-      res.status(404).json({ success: false, message: 'No product found!' });
+      return res.json({ success: true, data: productByID });
     }
+    return res.status(404).json({ success: false, message: 'No product found!' });
   } catch (err: any) {
     if (err.details) {
-      res.status(422).json({ msg: err.details[0].message, status: 422 });
+      return res.status(422).json({ msg: err.details[0].message, status: 422 });
     }
     return next(err);
   }
