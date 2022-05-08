e69e074c0490e70f717c2d197709ebd12fdb2119
MahmoudJD95 <mld95.doa@gmail.com>
2022-05-08 15:39:32 +0300

    fix:remove transaction

--- server/src/controllers/orders/index.ts
+++ server/src/controllers/orders/index.ts
@@ -1,6 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
 import {
-  transaction,
   literal,
 } from 'sequelize';
 import { Order, ProductOrder, Product } from '../../database';
@@ -25,19 +24,17 @@ const addOrder = async (req:ModRequest, res:Response, next:NextFunction) => {
   const isAdmin = req.user?.isAdmin || false;
   if (isSupplied && !isAdmin) return next(CustomizedError('Bad Request', 400));
 
-  const t = await transaction();
-
   try {
     await orderValidation(req);
     const order = await Order.create({
       date, totalPrice: Infinity, paidPrice, status: 'pending', isSupplied,
-    }, { transaction: t });
+    });
     await ProductOrder.bulkCreate(productArray.map(({ id, quantity }:
       {id:number, quantity:number}) => ({
       productId: id,
       orderId: order.id,
       quantity: isSupplied ? -quantity : quantity,
-    })), { transaction: t });
+    })));
 
     const totalPrice = await ProductOrder.findAll({
       where: { orderId: order.id },
@@ -54,10 +51,9 @@ const addOrder = async (req:ModRequest, res:Response, next:NextFunction) => {
         [literal('SUM(product.price*quantity)'), 'totalPrice'],
       ],
       group: ['productOrder.orderId'],
-      transaction: t,
     });
     order.totalPrice = Number(totalPrice[0].totalPrice);
-    await order.save({ transaction: t });
+    await order.save();
 
     return res.status(200).json({ message: 'Order Added Successfully !' });
   } catch (err:any) {
