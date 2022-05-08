e2891e826eff641b8a777fc6b0658fcf8f3f877e
yousra Jaradh <yousrakhaleel@gmail.com>
2022-05-08 10:58:36 +0300

    test: add console.log to track error

--- server/src/controllers/serverError.ts
+++ server/src/controllers/serverError.ts
@@ -5,6 +5,7 @@ interface ModError extends Error {
 }
 // eslint-disable-next-line no-unused-vars
 const serverError = (err:ModError, req:Request, res:Response, next:NextFunction) => {
+  console.log(err);
   res.status(err.status || 500).json({ status: (err.status || 500), msg: err.message });
 };
 
