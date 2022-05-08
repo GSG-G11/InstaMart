348bf6f961a552a548eb5e2d542106856ae8aeb8
Ibrahim-Jarada <ibrahim.jarada@gmail.com>
2022-05-08 16:11:13 +0300

    build: create a transaction to reverse effect if any query fails relates #78

--- server/src/controllers/orders/index.ts
+++ server/src/controllers/orders/index.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import {
   literal,
 } from 'sequelize';
-import { Order, ProductOrder, Product } from '../../database';
+import sequelize, { Order, ProductOrder, Product } from '../../database';
 import { orderValidation } from '../validation';
 import { CustomizedError } from '../../utilities';
 
@@ -23,18 +23,18 @@ const addOrder = async (req:ModRequest, res:Response, next:NextFunction) => {
 
   const isAdmin = req.user?.isAdmin || false;
   if (isSupplied && !isAdmin) return next(CustomizedError('Bad Request', 400));
-
+  const t = await sequelize.transaction();
   try {
     await orderValidation(req);
     const order = await Order.create({
       date, totalPrice: Infinity, paidPrice, status: 'pending', isSupplied,
-    });
+    }, { transaction: t });
     await ProductOrder.bulkCreate(productArray.map(({ id, quantity }:
       {id:number, quantity:number}) => ({
       productId: id,
       orderId: order.id,
       quantity: isSupplied ? -quantity : quantity,
-    })));
+    })), { transaction: t });
 
     const totalPrice = await ProductOrder.findAll({
       where: { orderId: order.id },
@@ -51,12 +51,14 @@ const addOrder = async (req:ModRequest, res:Response, next:NextFunction) => {
         [literal('SUM(product.price*quantity)'), 'totalPrice'],
       ],
       group: ['productOrder.orderId'],
+      transaction: t,
     });
     order.totalPrice = Number(totalPrice[0].totalPrice);
-    await order.save();
+    await order.save({ transaction: t });
 
     return res.status(200).json({ message: 'Order Added Successfully !' });
   } catch (err:any) {
+    await t.rollback();
     if (err.details) {
       return res.status(422).json({ msg: err.details[0].message, status: 422 });
     }
