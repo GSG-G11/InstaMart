1b87d994774b681459812f97e2a28a806868eeeb
SalmaIssa96 <salmaissa196@gmail.com>
2022-05-10 00:57:54 +0300

    feat: add slider to latest products section

--- client/src/Components/latestProduct/index.jsx
+++ client/src/Components/latestProduct/index.jsx
@@ -2,6 +2,9 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Alert, CircularProgress } from '@mui/material';
 import './style.css';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import Card from '../Card';
 
 function LatestProducts() {
@@ -40,7 +43,15 @@ function LatestProducts() {
     '#0dcaf0',
     '#ffc107',
     '#dc3545',
-    '#212529'];
+    '#212529',
+  ];
+
+  const settings = {
+    infinite: false,
+    speed: 500,
+    slidesToShow: products.length < 4 ? products.length : 4,
+    slidesToScroll: 3,
+  };
 
   return (
     <div>
@@ -49,19 +60,37 @@ function LatestProducts() {
         <div className="latest-products-section-cards">
           {isLoading ? (
             <CircularProgress sx={{ color: '#3bb77e' }} />
-          ) : products.slice(0, 5).map(({
-            id, name, price, imageUrl, category: { name: category },
-          }) => (
-            <Card
-              key={id}
-              id={id}
-              name={name}
-              price={+price}
-              imageUrl={imageUrl}
-              category={category}
-              color={colorsArr[id % colorsArr.length]}
-            />
-          ))}
+          ) : (
+            <Slider
+              className="products-slider"
+              infinite={settings.infinite}
+              speed={settings.speed}
+              slidesToShow={settings.slidesToShow}
+              slidesToScroll={settings.slidesToScroll}
+            >
+              {products
+                .slice(0, 20)
+                .map(
+                  ({
+                    id,
+                    name,
+                    price,
+                    imageUrl,
+                    category: { name: category },
+                  }) => (
+                    <Card
+                      key={id}
+                      id={id}
+                      name={name}
+                      price={+price}
+                      imageUrl={imageUrl}
+                      category={category}
+                      color={colorsArr[id % colorsArr.length]}
+                    />
+                  ),
+                )}
+            </Slider>
+          )}
         </div>
       </div>
       {errorAlert ? (
