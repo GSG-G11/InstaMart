3b5416b9cf78801a9b0a8d88655a0d2a2a08ed0b
SalmaIssa96 <salmaissa196@gmail.com>
2022-05-14 02:06:10 +0300

    fix: import css file in footer.jsx

--- client/src/Components/Footer/index.jsx
+++ client/src/Components/Footer/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {
   Instagram, Phone, Twitter, FacebookOutlined, WhatsApp,
 } from '@mui/icons-material';
+import './Footer.css';
 
 function Footer() {
   return (
