e1943084d56726893b5f99476a5bf0ac5db673c5
SalmaIssa96 <salmaissa196@gmail.com>
2022-05-17 03:08:07 +0300

    feat: create web socket server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,14 @@
 /* eslint-disable no-console */
+import { Server } from 'socket.io';
+import { Server as HttpServer } from 'http';
 import app from './app';
 
-const port:Number = app.get('port');
+const port: Number = app.get('port');
+const httpServer = new HttpServer(app);
 
-app.listen(port, () => {
+httpServer.listen(port, () => {
   console.log(`server is running on http://localhost:${port}/api/v1`);
 });
+
+const socketServer = new Server(httpServer, { cors: { origin: '*' } });
+app.set('socketio', socketServer);
